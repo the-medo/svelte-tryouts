16e2252fdbdb0692764b2a0144f4023d21198fec
Medo <martinmederly@gmail.com>
2023-12-22 16:49:11 +0100

    feat: user management

---
+++ src/lib/components/AuthCheck.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { auth, user } from '$lib/firebase';
+	import { signOut } from 'firebase/auth';
+</script>
+
+{#if $user}
+	<slot/>
+{:else}
+	<p class="text-error">
+		You must be signed in to view this page.
+		<a class="btn btn-primary" href="/login">Sign in</a>
+	</p>
+{/if}
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged, type User } from "firebase/auth";
 import { getStorage } from "firebase/storage";
+import { writable } from 'svelte/store';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,3 +22,30 @@ export const app = initializeApp(firebaseConfig);
 export const db = getFirestore()
 export const auth = getAuth()
 export const storage = getStorage()
+
+function userStore() {
+  let unsubscribe: () => void;
+
+  if (!auth || !globalThis.window) {
+    console.warn("Auth is not initialized or not in browser")
+    const {subscribe}  = writable<User | null>(null)
+
+    return {
+      subscribe
+    };
+  }
+
+  const { subscribe } = writable(auth?.currentUser ?? null, (set) => {
+    onAuthStateChanged(auth, (user) => {
+      set(user)
+    });
+
+    return () => unsubscribe()
+  });
+
+  return {
+    subscribe
+  };
+}
+
+export const user = userStore();
